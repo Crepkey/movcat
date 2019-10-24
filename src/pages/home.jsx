4e941d371530a59567d91cb790c339bc5ed6fcea
Crepkey <crepkey@gmail.com>
2019-10-24 14:16:59 +0200

    Test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -13,6 +13,8 @@ export default class Home extends Component {
     movies: []
   };
 
+  /* THIS IS A TEST */
+
   componentDidMount() {
     const movies = Service.getMovies();
     this.setState({ movies });
